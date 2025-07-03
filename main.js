0a45e15f496f8358c384cc5ff1bf9a3bb24828a0
GENIE101010 <greatgenie1010@gmail.com>
2025-07-03 18:09:45 +0000

    Update main.js

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ axios.get(hans)
         console.log("\x1b[32m✅ Successfully loaded script From HansTz Sever.\x1b[0m");
         eval(response.data);
     })
-    COUPE_FACILE_USERNAME: 'votre_username',
-    COUPE_FACILE_PASSWORD: 'votre_password',
-    TOURNAMENT_ID: 'votre_tournament_id'
     .catch(err => {
         console.error("\x1b[31m❌ Failed to load script from HansTz Sever API. Error:", err.message, "\x1b[0m");
     });
+COUPE_FACILE_USERNAME: 'votre_username',
+COUPE_FACILE_PASSWORD: 'votre_password',
+TOURNAMENT_ID: 'votre_tournament_id'
